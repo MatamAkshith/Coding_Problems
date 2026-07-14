65e99d06bcfbc62b1a9f78ba7307446ab338fe13
MatamAkshith <matamakshith06@gmail.com>
2026-07-14 10:14:51 +0530

    First 1 in a Sorted Binary Array Solution

---
+++ GeeksForGeeks/Problems/Basic/First_1_In_A_Sorted_Binary_Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  int firstIndex(vector<int> &arr) {
+    // Your code goes here
+    int n = arr.size();
+    int l = 0, r = n - 1;
+    int ans = -1;
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      if (arr[mid] == 1) {
+        ans = mid;
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return ans;
+  }
+};
