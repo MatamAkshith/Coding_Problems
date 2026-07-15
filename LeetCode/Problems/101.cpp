69f19f527fb745aa863621654c5f3332155eb065
MatamAkshith <matamakshith06@gmail.com>
2026-07-15 11:42:45 +0530

    Symmetric Tree Optimal Solution

--- LeetCode/Problems/101.cpp
+++ LeetCode/Problems/101.cpp
@@ -10,31 +10,50 @@
  * right(right) {}
  * };
  */
+// class Solution {
+// public:
+//   bool flag;
+//   bool isSymmetric(TreeNode *root) {
+//     flag = traversal(root->left, root->right);
+//     return flag;
+//   }
+//   bool traversal(TreeNode *p, TreeNode *q) {
+//     if (!p && !q) {
+//       return true;
+//     }
+//     if (!p || !q) {
+//       return false;
+//     }
+//     flag = traversal(p->left, q->right);
+//     if (!flag) {
+//       return false;
+//     }
+//     if (p->val != q->val) {
+//       return false;
+//     }
+//     flag = traversal(p->right, q->left);
+//     if (!flag) {
+//       return false;
+//     }
+//     return flag;
+//   }
+// };
 class Solution {
 public:
-  bool flag;
-  bool isSymmetric(TreeNode *root) {
-    flag = traversal(root->left, root->right);
-    return flag;
-  }
-  bool traversal(TreeNode *p, TreeNode *q) {
-    if (!p && !q) {
-      return true;
+    bool isSymmetric(TreeNode* root) {
+        bool flag = traversal(root->left, root->right);
+        return flag;
     }
-    if (!p || !q) {
-      return false;
+    bool traversal(TreeNode * p, TreeNode * q){
+        if(!p && !q){
+            return true;
+        }
+        if(!p || !q){
+            return false;
+        }
+        if(p->val != q->val){
+            return false;
+        }
+        return traversal(p->left, q->right) && traversal(p->right, q->left);
     }
-    flag = traversal(p->left, q->right);
-    if (!flag) {
-      return false;
-    }
-    if (p->val != q->val) {
-      return false;
-    }
-    flag = traversal(p->right, q->left);
-    if (!flag) {
-      return false;
-    }
-    return flag;
-  }
 };
