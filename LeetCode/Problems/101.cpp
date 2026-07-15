1e2f0293dea487bf225895e749ad423585e9d372
MatamAkshith <matamakshith06@gmail.com>
2026-07-15 11:40:42 +0530

    Symmetric Tree Solution (Original Approach)

---
+++ LeetCode/Problems/101.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  bool flag;
+  bool isSymmetric(TreeNode *root) {
+    flag = traversal(root->left, root->right);
+    return flag;
+  }
+  bool traversal(TreeNode *p, TreeNode *q) {
+    if (!p && !q) {
+      return true;
+    }
+    if (!p || !q) {
+      return false;
+    }
+    flag = traversal(p->left, q->right);
+    if (!flag) {
+      return false;
+    }
+    if (p->val != q->val) {
+      return false;
+    }
+    flag = traversal(p->right, q->left);
+    if (!flag) {
+      return false;
+    }
+    return flag;
+  }
+};
