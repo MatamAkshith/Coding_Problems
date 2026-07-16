f79316e12eb69526da5ec10f691b1906d209eef2
MatamAkshith <matamakshith06@gmail.com>
2026-07-16 10:23:56 +0530

    Binary Tree PreOrder Traversal (Iterative Approach) Solution

--- LeetCode/Problems/144.cpp
+++ LeetCode/Problems/144.cpp
@@ -10,19 +10,52 @@
  * right(right) {}
  * };
  */
+// class Solution {
+// public:
+//   vector<int> res;
+//   vector<int> preorderTraversal(TreeNode *root) {
+//     traversal(root);
+//     return res;
+//   }
+//   void traversal(TreeNode *node) {
+//     if (!node) {
+//       return;
+//     }
+//     res.push_back(node->val);
+//     traversal(node->left);
+//     traversal(node->right);
+//   }
+// };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
   vector<int> res;
+  stack<TreeNode *> s;
   vector<int> preorderTraversal(TreeNode *root) {
-    traversal(root);
-    return res;
-  }
-  void traversal(TreeNode *node) {
-    if (!node) {
-      return;
+    if (!root) {
+      return res;
     }
-    res.push_back(node->val);
-    traversal(node->left);
-    traversal(node->right);
+    s.push(root);
+    while (!s.empty()) {
+      root = s.top();
+      s.pop();
+      res.push_back(root->val);
+      if (root->right)
+        s.push(root->right);
+      if (root->left)
+        s.push(root->left);
+    }
+    return res;
   }
 };
