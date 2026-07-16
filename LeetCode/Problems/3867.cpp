0af41ddb8b4de40537f5ea0d590143d98cf619e6
MatamAkshith <matamakshith06@gmail.com>
2026-07-16 10:00:00 +0530

    Sum of GCD of Formed Pairs (Optimal Approach) Solution

--- LeetCode/Problems/3867.cpp
+++ LeetCode/Problems/3867.cpp
@@ -1,25 +1,42 @@
+// class Solution {
+// public:
+//   long long gcd(long long a, long long b) {
+//     if (a == 0) {
+//       return b;
+//     }
+//     return gcd(b % a, a);
+//   }
+//   long long gcdSum(vector<int> &nums) {
+//     int n = nums.size();
+//     int maxi = 0;
+//     vector<int> prefixGcd;
+//     for (int i = 0; i < n; i++) {
+//       maxi = max(maxi, nums[i]);
+//       prefixGcd.push_back(gcd(nums[i], maxi));
+//     }
+//     sort(prefixGcd.begin(), prefixGcd.end());
+//     int r = n - 1;
+//     long long sum = 0;
+//     for (int i = 0; i < n / 2; i++) {
+//       sum += gcd(prefixGcd[i], prefixGcd[r]);
+//       r--;
+//     }
+//     return sum;
+//   }
+// };
 class Solution {
 public:
-  long long gcd(long long a, long long b) {
-    if (a == 0) {
-      return b;
-    }
-    return gcd(b % a, a);
-  }
   long long gcdSum(vector<int> &nums) {
     int n = nums.size();
     int maxi = 0;
-    vector<int> prefixGcd;
-    for (int i = 0; i < n; i++) {
-      maxi = max(maxi, nums[i]);
-      prefixGcd.push_back(gcd(nums[i], maxi));
-    }
-    sort(prefixGcd.begin(), prefixGcd.end());
-    int r = n - 1;
     long long sum = 0;
-    for (int i = 0; i < n / 2; i++) {
-      sum += gcd(prefixGcd[i], prefixGcd[r]);
-      r--;
+    for (int &x : nums) {
+      maxi = max(x, maxi);
+      x = gcd(x, maxi);
+    }
+    sort(nums.begin(), nums.end());
+    for (int l = 0, r = n - 1; l < r; l++, r--) {
+      sum += gcd(nums[l], nums[r]);
     }
     return sum;
   }
