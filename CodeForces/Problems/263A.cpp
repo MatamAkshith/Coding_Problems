5fb83b9bfc27da0477d0f0dad7f0e0d4685f2859
MatamAkshith <matamakshith06@gmail.com>
2026-07-21 14:18:57 +0530

    Add solution of CodeForces 50A

---
+++ CodeForces/Problems/263A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<vector<int>> arr(5, vector < int > 5);
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; i < 5; j++) {
+      cin >> arr[i][j];
+    }
+  }
+  int a, b;
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (arr[i][j] == 1) {
+        a = i;
+        b = j;
+      }
+    }
+  }
+  int res = abs(a - 2) + abs(b - 2);
+  cout << res << '\n';
+  return 0;
+}
