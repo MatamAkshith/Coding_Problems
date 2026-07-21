f2ec5c16772db17c8eecac3dbc3385de32cbf530
MatamAkshith <matamakshith06@gmail.com>
2026-07-21 14:26:53 +0530

    Add solution of CodeForces 263A

--- CodeForces/Problems/263A.cpp
+++ CodeForces/Problems/263A.cpp
@@ -2,22 +2,15 @@
 using namespace std;
 
 int main() {
-  vector<vector<int>> arr(5, vector < int > 5);
+  vector<vector<int>> arr(5, vector<int>(5));
   for (int i = 0; i < 5; i++) {
     for (int j = 0; j < 5; j++) {
       cin >> arr[i][j];
-    }
-  }
-  int a, b;
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
       if (arr[i][j] == 1) {
-        a = i;
-        b = j;
+        cout << abs(i - 2) + abs(j - 2) << endl;
+        return 0;
       }
     }
   }
-  int res = abs(a - 2) + abs(b - 2);
-  cout << res << endl;
   return 0;
 }
