860b6d859dc93786b527b414510f754a3da4c66e
MatamAkshith <matamakshith06@gmail.com>
2026-08-04 14:12:14 +0530

    Find Missing Elements (Own approach)

---
+++ LeetCode/Problems/3731.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+        unordered_set<int> s;
+        s.insert(nums.begin(), nums.end());
+        int mini = nums[0], maxi = nums[0];
+        for (int i = 1; i < n; i++) {
+            mini = min(mini, nums[i]);
+            maxi = max(maxi, nums[i]);
+        }
+        for (int i = mini; i <= maxi; i++) {
+            if (s.find(i) == s.end()) {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
