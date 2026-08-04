63b8ee0b55659c5d314b4f8d0535ad3251a10f2a
MatamAkshith <matamakshith06@gmail.com>
2026-08-04 14:17:50 +0530

    Stone Game (Optimal Approach)

--- LeetCode/Problems/877.cpp
+++ LeetCode/Problems/877.cpp
@@ -1,15 +1,19 @@
+// class Solution {
+//    public:
+//     int solve(vector<int>& piles, int i, int j) {
+//         if (i == j) return piles[i];
+
+//         int left = piles[i] - solve(piles, i + 1, j);
+//         int right = piles[j] - solve(piles, i, j - 1);
+
+//         return max(left, right);
+//     }
+
+//     bool stoneGame(vector<int>& piles) {
+//         return solve(piles, 0, piles.size() - 1) > 0;
+//     }
+// };
 class Solution {
    public:
-    int solve(vector<int>& piles, int i, int j) {
-        if (i == j) return piles[i];
-
-        int left = piles[i] - solve(piles, i + 1, j);
-        int right = piles[j] - solve(piles, i, j - 1);
-
-        return max(left, right);
-    }
-
-    bool stoneGame(vector<int>& piles) {
-        return solve(piles, 0, piles.size() - 1) > 0;
-    }
+    bool stoneGame(vector<int>& piles) { return true; }
 };
