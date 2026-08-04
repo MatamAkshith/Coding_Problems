3bf264dccd1f9d481fac846703a4d8160cb6d689
MatamAkshith <matamakshith06@gmail.com>
2026-08-04 14:30:51 +0530

    Maximum Subarray (Kadabes Approach)

--- LeetCode/Problems/53.cpp
+++ LeetCode/Problems/53.cpp
@@ -1,21 +1,33 @@
+// class Solution {
+//    public:
+//     int maxSubArray(vector<int>& nums) {
+//         int n = nums.size();
+//         int maxi = INT_MIN, sum = 0;
+//         if (n == 0) {
+//             return maxi;
+//         }
+//         if (n == 1) {
+//             return nums[0];
+//         }
+//         for (int i = 0; i < n; i++) {
+//             sum += nums[i];
+//             maxi = max(maxi, sum);
+//             if (sum < 0) {
+//                 sum = 0;
+//             }
+//         }
+//         return maxi;
+//     }
+// };
 class Solution {
    public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int maxi = INT_MIN, sum = 0;
-        if (n == 0) {
-            return maxi;
+        int curr = nums[0], ans = nums[0];
+        for (int i = 1; i < n; i++) {
+            curr = max(nums[i], curr + nums[i]);
+            ans = max(curr, ans);
         }
-        if (n == 1) {
-            return nums[0];
-        }
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            maxi = max(maxi, sum);
-            if (sum < 0) {
-                sum = 0;
-            }
-        }
-        return maxi;
+        return ans;
     }
 };
