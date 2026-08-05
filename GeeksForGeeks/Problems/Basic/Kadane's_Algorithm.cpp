7b64ee9e6f215661a410249b82b9c24bce9fe3bd
MatamAkshith <matamakshith06@gmail.com>
2026-08-05 14:22:34 +0530

    Kadane's Algorithm Solution

---
+++ GeeksForGeeks/Problems/Basic/Kadane's_Algorithm.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    int maxSubarraySum(vector<int>& arr) {
+        // Code here
+        int n = arr.size();
+        int maxi = INT_MIN;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            maxi = max(sum, maxi);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+};
