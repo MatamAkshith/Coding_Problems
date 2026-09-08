dfa0a3864bfe0957285307d0b49c1a3decd6936b
MatamAkshith <matamakshith06@gmail.com>
2026-09-08 11:43:04 +0530

    Stock Buy and Sell – Multiple Transaction Allowed Solution

---
+++ GeeksForGeeks/Problems/Medium/Stock_Buy_and_Sell–Multiple_Transaction_Allowed.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        // code here
+        int n = prices.size();
+        int profit = 0;
+        int mini = prices[0];
+        int maxi = 0;
+        for (int i = 1; i < n; i++) {
+            int curr = 0;
+            mini = min(mini, prices[i]);
+            curr = max(curr, prices[i] - mini);
+            mini = prices[i];
+            maxi += curr;
+            curr = 0;
+        }
+        return maxi;
+    }
+};
